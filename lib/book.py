1494acce6321e8e5ecb8a04c8e75800eb2dab467
Kyle Greenidge <kylegreen149@gmail.com>
2024-09-04 16:28:07 -0400

    Done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,39 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self, title = None, page_count = None):
+        self.title = title
+        self.page_count = page_count
+
+    def get_title(self):
+        return self._title
+
+    def set_title(self, new_title):
+        if isinstance(new_title, str):
+            print(new_title)
+            self._title = new_title
+        else:
+            print("Enter a new title")
+
+    title = property(get_title, set_title)
+
+
+    def get_page(self):
+        return self._page_count
+
+    def set_page(self, new_page_count):
+        if isinstance(new_page_count, int) and (new_page_count >= 0):
+            # print("Flipping the page...wow, you read fast!")
+            # new_page_count = new_page_count + 1
+            self._page_count = new_page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+        self._page_count + 1
+
+    page_count = property(get_page, set_page, turn_page)
+
+b1 = Book("Harry Potter", 30)
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand = None, size = None):
+        self.brand = brand
+        self.size = size
+        
+
+    def get_brand(self):
+        return self._brand
+
+    def set_brand(self, new_brand):
+        if isinstance(new_brand, str):
+            print(new_brand)
+            self._brand = new_brand
+        else:
+            print("Enter a new shoe brand")
+
+    brand = property(get_brand, set_brand)
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, new_size):
+        if isinstance(new_size, int) and (new_size > 0):
+            self._size = new_size
+        else:
+            print("size must be an integer")
+
+
+    def cobble(self, condition = "New"):
+        print("Your shoe is as good as new!")
+        self.condition = condition
+
+    size = property(get_size, set_size, cobble)
